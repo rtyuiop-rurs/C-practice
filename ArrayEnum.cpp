d73a0c80054c38efbbf3b2eab49841fb55022c40
Adam_Aqil <yuiopqwerty555@gmail.com>
2025-11-03 13:38:24 +0800

    testing and practising arrays with enums

---
+++ ArrayEnum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<cassert>
+
+namespace Animals{
+    enum animals{
+        chicken, 
+        dog,
+        cat,
+        elephant,
+        duck,
+        snake,
+        animalcount,
+    };
+}
+
+
+std::vector legs{2,4,4,4,2,0};
+
+
+int main(){
+    assert(std::size(legs) == Animals::animalcount);
+    std::cout<<legs[Animals::elephant];
+    std::cout<<legs[Animals::chicken];
+}
