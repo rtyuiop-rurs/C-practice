422bd0414a67aa0255a5279a083ff0a6445f802d
Adam_Aqil <yuiopqwerty555@gmail.com>
2025-11-17 20:06:20 +0800

    new changes

--- C-Arrays6.cpp
+++ C-Arrays6.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
 
-constexpr int square[] {0, 1, 4, 9};
+constexpr int square[] {0,1,4,9};
 
-void getTrueNUm(int answer){
-    const int index{4};
+void getAnswer(int answer){
     bool correct{false};
-    for(std::size_t i = 0 ; i < 4; i++ ){
+    constexpr int index{4};
+    for(std::size_t i = 0; i < index; i++){
         if(square[i] == answer){
             correct = true;
-            std::cout<<answer<<" is a perfect square";
+            std::cout<<answer<<" is a perfect square!\n";
         }
     }
-    if(!correct && answer != -1){
-        std::cout<<answer<<" is not a perfect square";
+
+    if(!correct  && answer != -1){
+        std::cout<<answer<<" is not a perfect square!\n";
     }
 }
 
 int main(){
     int answer{0};
     do{
-        std::cout<<"Enter a number (-1) to quit: ";
+        std::cout<<"Enter a number (-1 to exit): ";
         std::cin>>answer;
-        getTrueNUm(answer);
-
+        getAnswer(answer);
     }while(answer != -1);
+
+    return 0;
 }
