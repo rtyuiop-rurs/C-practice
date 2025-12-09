2afc38c3f166045287af3fb02ec676228da3f219
Adam_Aqil <yuiopqwerty555@gmail.com>
2025-12-09 17:44:53 +0800

    minor changes

--- RecursiveFunct.cpp
+++ RecursiveFunct.cpp
@@ -1,4 +1,12 @@
 #include<iostream>
+#include<functional>
+
+int choice(){
+    std::cout<<"Enter your choice <1 for factorial, 2 for sum>: ";
+    int choice{0};
+    std::cin>>choice;
+    return choice;
+}
 
 int recursive(int x){
     int total{0};
@@ -11,10 +19,29 @@ int recursive(int x){
     return total + recursive(last_num) ;
 }
 
+int factorial(int x){
+    if(x == 0){
+        return 1;
+    }
+
+    return x + factorial(x - 1);
+}
+
+using ArithmeticFunction = std::function <int(int)>;
+
+ArithmeticFunction getFunction(int x){
+    switch(x){
+        case 1 : return &factorial;
+        case 2 : return &recursive;
+        default : return nullptr;
+    }
+}
+
 int main(){
     std::cout<<"Enter your number: ";
     int num{0};
     std::cin>>num;
-    int total{recursive(num)};
-    std::cout<<total;
+    int ch{choice()};
+    ArithmeticFunction fcn{getFunction(ch)};
+    std::cout<<fcn(num);
 }
