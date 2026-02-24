196cc1175a30bb87103d2daa40abf70fd3b4d2ae
Adam_Aqil <yuiopqwerty555@gmail.com>
2026-02-24 21:42:53 +0800

    new practices

--- test2.cpp
+++ test2.cpp
@@ -1,15 +1,41 @@
 #include<iostream>
+int getSum(int last_num){
+    int sum{0};
+    int i = 0;
+    while(i < last_num){
+        i++;
+        sum += i;
+    }
+
+    return sum;
+}
+
+int getFactorialSum(int last_num){
+    int sum{1};
+    int i = 1;
+    while(i < last_num){
+        i++;
+        sum *= i;
+    }
+
+    return sum;
+}
+
 
 int main(){
     std::cout<<"Last number?: ";
     int number;
     std::cin>>number;
-    int sum = 1;
-    int i = 0;
-    while( i < number){
-        i++;
-        sum *= i;
-    }
-    std::cout<<"Sum factorial: "<<sum;
+    char choice{'?'};
+    std::cout<<"Is factorial?: ";
+    std::cin>>choice;
+    int sum{0};
 
+    if(choice == 'y'){
+        sum = getFactorialSum(number);
+    }else{
+       sum =  getSum(number);
+    }
+
+    std::cout<<sum<<"\n";
 }
